19e2f72ee6d8c354943a055cf835a3671afe8303
jeonyeohun <21500630@handong.edu>
2020-05-12 19:25:09 +0900

    finalize HW4

--- assignments/assignment4/bnb.c
+++ assignments/assignment4/bnb.c
@@ -13,17 +13,6 @@ int dp [2][400001];
 int heap_size=0;
 clock_t start, end;
 
-double getExecuteTime(){
-  end = clock();
-  double diff = end - start;
-  return diff/CLOCKS_PER_SEC*1000;
-}
-
-int isTimeOver(){
-  double time = getExecuteTime();
-  return time > TIME_LIMIT_MS;
-}
-
 typedef struct item {
     int benefit;
     int weight;
@@ -32,6 +21,17 @@ typedef struct item {
     int bound;
 }ITEM;
 ITEM itemLists [10001];
+
+double getExecuteTime(){
+  end = clock();
+  double diff = end - start;
+  return diff/CLOCKS_PER_SEC * 1000;
+}
+
+int isTimeOver(){
+  double time = getExecuteTime();
+  return time > TIME_LIMIT_MS;
+}
 
 /*-------- priority queue related functions --------- */
 void exchange (ITEM heap [], int a, int b){
@@ -62,9 +62,9 @@ void push(ITEM heap[], ITEM num){
     int i = heap_size;
     
     heap[i] = num;
-    while(i > 1 && heap[i/2].bound < heap[i].bound){
-        exchange(heap, i, i/2);
-        i = i/2;
+    while(i > 1 && heap[i / 2].bound < heap[i].bound){
+        exchange(heap, i, i / 2);
+        i = i / 2;
     }
 }
 
@@ -94,13 +94,13 @@ int static compare (const void * first, const void *second){
 
 /* Using Greeady Algorithm Approach */
 double greedy(ITEM * itemList, int N , int maxW){
-    qsort(itemList+1, N+1, sizeof(ITEM), compare);
+    qsort(itemList + 1, N + 1, sizeof(ITEM), compare);
     int curW = 0;
     double maxBenefit = 0;
-    for (int i = 1 ; i <=N ; i++){
+    for (int i = 1 ; i <= N ; i++){
         if (isTimeOver()) return -1;
         if (curW + itemList[i].weight > maxW){
-            maxBenefit += (double)(itemList[i].bpw) * (maxW-curW);
+            maxBenefit += (double)(itemList[i].bpw) * (maxW - curW);
             break;
         }
         curW += itemList[i].weight;
@@ -111,24 +111,28 @@ double greedy(ITEM * itemList, int N , int maxW){
 }
 
 /* Using Dynamic Programming Approach */
-int dpK(ITEM * itemList, int N, int W){
-  int maxbenefit = 0;
-  for (int i = 1 ; i <= W ; i++) dp[0][i] = 0;
-    for (int i = 1 ; i <= N ; i++){
+int slidingDP(ITEM * itemList, int N, int W){
+  int maxBenefit = 0;
+  for (int i = 1 ; i <= W ; i++) {
+      dp[0][i] = 0;
+  }
+
+  for (int i = 1 ; i <= N ; i++){
       if (isTimeOver()) return -1;
-        for(int w = 0 ; w <= W ; w++){
+      for(int w = 0 ; w <= W ; w++){
           int wi = itemList[i].weight;
           int bi = itemList[i].benefit;
-            if(w < wi) dp[i%2][w] = dp[1-i%2][w];
-            else{
-              if (dp[1-i%2][w] > dp[1-i%2][w-wi]+bi) dp[i%2][w] = dp[1-i%2][w];
-              else dp[i%2][w] = dp[1-i%2][w-wi]+bi;
+          if(w < wi) dp[i % 2][w] = dp[1 - i % 2][w];
+          else{
+              if (dp[1 - i % 2][w] > dp[1 - i % 2][w - wi] + bi) {
+                  dp[i % 2][w] = dp[1 - i % 2][w];
+              }
+              else dp[i % 2][w] = dp[1 - i % 2][w - wi] + bi;
             }
-            if (maxbenefit < dp[i%2][w])maxbenefit = dp[i%2][w];
+            if (maxBenefit < dp[i % 2][w]) maxBenefit = dp[i % 2][w];
         }
     }
-    return maxbenefit;
-    
+    return maxBenefit;    
 }
 
 /* calculate bound of each node  */
@@ -140,7 +144,7 @@ int calcBound (ITEM * itemList, ITEM node, int N, int W){
 
     else{
         bound = node.benefit;
-        while(level<=N && totWeight + itemList[level].weight <= W){
+        while(level <= N && totWeight + itemList[level].weight <= W){
             totWeight += itemList[level].weight;
             bound += itemList[level].benefit;
             level++;
@@ -160,13 +164,11 @@ int bandb(ITEM * itemList, int N, int W){
     root.level = 0;
     root.bound = calcBound(itemList,root, N, W);
     heap_size = 0;
-int maxheap = 0;    
     int maxBenefit = 0;
     push(pq, root);
 
     while (heap_size != 0){
-if(maxheap < heap_size)maxheap = heap_size;
-if (isTimeOver()) return -1;
+        if (isTimeOver()) return -1;
         ITEM node = pop(pq);
         ITEM left, right = node;
         left.level = node.level+1;
@@ -183,19 +185,18 @@ if (isTimeOver()) return -1;
             maxBenefit = left.benefit;
         }
         
-        left.bound =calcBound(itemList, left, N, W);
+        left.bound = calcBound(itemList, left, N, W);
         if (left.bound > maxBenefit){
             push(pq, left);
         }
       
         right.bound = calcBound(itemList, node, N, W);
-        if (right.bound >maxBenefit){
+        if (right.bound > maxBenefit){
             right.level = node.level + 1;
             push(pq, right);
         }
         
     }
-printf("%d\n", maxheap);
     return maxBenefit;
 }
 
@@ -203,10 +204,10 @@ printf("%d\n", maxheap);
 /* Randomly Genrate Items */
 ITEM randGeneration(){
     ITEM i;
-    i.weight = rand()%100+1;
-    i.benefit = rand()%300+1;
+    i.weight = rand() % 100 + 1;
+    i.benefit = rand() % 300 + 1;
     i.level = 0;
-    i.bpw = (double)i.benefit/i.weight;
+    i.bpw = (double)i.benefit / i.weight;
 
     return i;
 }
@@ -215,50 +216,64 @@ int main (){
     srand((unsigned int)time(NULL));
     double time;
     FILE *fp = fopen("output.txt", "w");
-    fputs("# items\t | \t\t\t\t\tgreedy\t\t\t\t | \t\t\t\t\tDP\t\t\t\t\t | \t\t\t\t\tB&B\t\t\t\t\t |", fp);
-    fputs("\n=================================================================================\n", fp);
+    fputs("# items\t | \t\tgreedy\t\t| \t\tDP\t\t| \t\tB&B\t\t |", fp);
+    fputs("# items\t | \t\tgreedy\t\t | \t\t\tDP\t\t | \t\tB&B\t\t |", stdout);
+    fputs("\n===================================================================\n", fp);
+    fputs("\n===================================================================\n", stdout);
     fclose(fp);
     int teseCases [] = {10, 100, 500, 1000, 3000,5000, 7000, 9000, 10000};
     int N, W;
          
     for (int i = 0 ; i < sizeof(teseCases)/sizeof(teseCases[0]) ; i++){
       FILE *fp = fopen("output.txt", "a");
-	printf("%d\n", N);
         N = teseCases[i];
         W =  N * 40;
         /* generate random N items */
-   printf("allocate %d\n", N); 
        for (int i = 1 ; i <= N ; i++){
             itemLists[i] = randGeneration();
         }
         /* begin Greedy approach */
         start = clock();
-	printf("a");    
+	
         double result = greedy(itemLists, N, W);        
         time = getExecuteTime();
 
-        if (result == -1) fprintf(fp, "%5d\t\t time over", N);
-        else fprintf(fp, "%5d\t\t |\t %6.3f ms / %.3f", N, time, result);
+        if (result == -1) {
+          fprintf(fp, "%5d\t\t time over\t\t", N);
+          fprintf(stdout, "%5d\t\t time over\t\t", N);
+        }
+        else {
+          fprintf(fp, "%6d\t |\t %.3f ms / %-.3f ", N, time, result);
+          fprintf(stdout, "%6d\t |\t %.3f ms / %.3f ", N, time, result);
+        }
 
-printf("b");
         /* begin DP approach */
         start = clock();
-        result = dpK(itemLists, N, W);
+        result = slidingDP(itemLists, N, W);
         time = getExecuteTime();
 
-        if (result == -1) fprintf(fp, "\ttime over");
-        else fprintf(fp, "\t\t%6.3f ms / %d  ", time, (int)result);
-printf("c");
+        if (result == -1) {
+          fprintf(fp, "\ttime over\t\t");
+          fprintf(stdout, "\ttime over\t\t");
+        }
+        else {
+          fprintf(fp, "\t\t%.3f ms / %-10d ", time, (int)result);
+          fprintf(stdout, "\t\t%.3f ms / %-10d ", time, (int)result);
+        }
+
         /* begin Branch and Bound approach */
         start = clock();
         result = bandb(itemLists, N, W);
         time = getExecuteTime();
 
-        if (result == -1) fprintf(fp, "\ttime over\n");
-        else fprintf(fp, "\t\t%6.3f ms / %d\n", time, (int)result);
-        
-        
-        printf("case %d done\n", N);
+        if (result == -1) {
+          fprintf(fp, "\ttime over\n");
+          fprintf(stdout, "\ttime over\n");
+        }
+        else {
+          fprintf(fp, "\t\t%.3f ms / %d \n ", time, (int)result);
+          fprintf(stdout, "\t\t%.3f ms / %d \n ", time, (int)result);
+        }
         fclose(fp);
     }
     return 0;
